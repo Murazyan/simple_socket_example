1cb5e8fbb83803ad4e62dc9fb17921db79049afa
MIC-8 <murazyan.margarita@mail.ru>
2018-06-25 18:00:16 +0400

    commit

---
+++ src/main/java/com/example/socket_example/SocketExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.socket_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketExampleApplication.class, args);
+    }
+}
